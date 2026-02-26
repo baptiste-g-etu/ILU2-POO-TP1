6618b94320884e5a8a9bdb277b3d58763e80add1
Nerf Magicarpe <baptiste.goudet@gmail.com>
2026-02-26 19:17:44 +0100

    End Part 1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -56,4 +56,101 @@ public class Village {
 		}
 		return chaine.toString();
 	}
+	
+	// TP starts here
+	private static class Marche {
+		private Etal[] etals;
+		public Marche(int nbEtals) {
+			this.etals = new Etal[nbEtals];
+			for(int i=0; i<nbEtals; ++i) {
+				etals[i] = new Etal();
+			}
+		}
+		
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			if(!(etals[indiceEtal].isEtalOccupe())) {
+				etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+			}
+		}
+	
+		private int trouverEtalLibre() {
+			for(int i=0; i<etals.length; ++i) {
+				if(!etals[i].isEtalOccupe()) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		private Etal[] trouverEtals(String produit) {
+			Etal[] tab;
+			int nbEtals = 0;
+			// Find exact number of Etal
+			for(int i=0; i < etals.length; ++i) {
+				if(etals[i].contientProduit(produit)) {
+					++nbEtals;
+				}
+			}
+
+			// Fill the table
+			if(nbEtals == 0) {
+				return null;
+			}
+			
+			tab = new Etal[nbEtals];
+			int indice = 0;
+			for(int i=0; i < etals.length; ++i) {
+				if(etals[i].contientProduit(produit)) {
+					tab[indice] = etals[i];
+					++indice;
+				}
+			}
+			
+			return tab;
+		}
+		
+		private Etal trouverVendeur(Gaulois gaulois) {
+			for(int i=0; i<etals.length; ++i) {
+				// First check to avoid comparing a null string
+				if(etals[i].isEtalOccupe() && etals[i].getVendeur().getNom() == gaulois.getNom()) {
+					return etals[i];
+				}
+			}
+			return null;
+		}
+		
+		private String afficherMarcher() {
+			String result = "";
+			int nbEtalsVides = 0;
+			for(int i=0; i<etals.length; ++i) {
+				if(etals[i].isEtalOccupe()) {
+					result += etals[i].afficherEtal();
+				}else {
+					++nbEtalsVides;
+				}
+			}
+			result += "Il reste "+ nbEtalsVides + " étals non utilisés dans le marché.";
+			return result;
+			
+		}
+		
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
